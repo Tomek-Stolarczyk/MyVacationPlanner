6acf0aedd4d59edfe759d4969327a62a0c0b654b
Tomek-Stolarczyk <Tomek.stolarczyk9@gmail.com>
2021-11-21 22:45:37 -0500

    Huge commit but this commit also includes a django app and database schema

--- Source/SkyScanner_Service/skyscanner.py
+++ Source/SkyScanner_Service/skyscanner.py
@@ -1,15 +1,13 @@
 import requests
 
-url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsequotes/v1.0/US/USD/en-US/SFO-sky/JFK-sky/2021-10-30"
+url = "https://tequila-api.kiwi.com/locations/query?term=PRG&locale=en-US&location_types=airport&active_only=true"
 
-querystring = {"inboundpartialdate":"2019-12-01"}
 
 headers = {
-    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com",
-    'x-rapidapi-key': "b665dc1c0emsha3e12e786b85bc8p1c9c87jsna9c55d4a2f18"
+    'apikey': "HYUX_bmghSqzT1oO3nRCcz7-Qtz9ng3U"
     }
 
-response = requests.request("GET", url, headers=headers, params=querystring)
+response = requests.request("GET", url, headers=headers)
 
 print(response.text)
 if(response.status_code == 429):
